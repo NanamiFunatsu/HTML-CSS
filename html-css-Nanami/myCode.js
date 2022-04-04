2de62c2d46358e8d9fa84f9ba12bb6fd015804b0
nana <100113608+NanamiFunatsu@users.noreply.github.com>
2022-04-04 15:20:36 +0900

    add

--- html-css-Nanami/myCode.js
+++ html-css-Nanami/myCode.js
@@ -2,7 +2,9 @@
 // Please don't delete the 'use strict' line above
 // クリックイベント設定
 document.getElementById("sample").addEventListener("click", function () {
-  location.replace("https://techacademy.jp/");
+  location.replace("C:\Users\1338997\Documents\GitHub\test-prj\img\No1.png");
 }, false);
+
+
 
 
