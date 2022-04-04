dac79776bef1d3c8878ca5fa90f560c7e83e18ef
nana <100113608+NanamiFunatsu@users.noreply.github.com>
2022-04-04 20:22:19 +0900

    add

--- html-css-Nanami/myCode.js
+++ html-css-Nanami/myCode.js
@@ -2,7 +2,7 @@
 // Please don't delete the 'use strict' line above
 // クリックイベント設定
 document.getElementById("sample").addEventListener("click", function () {
-  location.replace("C:\Users\1338997\Documents\GitHub\test-prj\img\No1.png");
+  location.replace("C:\Users\1338997\Documents\GitHub\test-prj\img\unicorn.gif");
 }, false);
 
 
--- myCode.js
+++ myCode.js
@@ -1,8 +1,26 @@
 'use strict'
 // Please don't delete the 'use strict' line above
 // クリックイベント設定
-document.getElementById("sample").addEventListener("click", function () {
-  location.replace("https://techacademy.jp/");
-}, false);
+
+// document.getElementById("sample").addEventListener("click", function () {
+//   location.replace("https://cdn.dribbble.com/users/1281272/screenshots/4515441/unicorn.gif");
+// }, false);
 
 
+// const target = document.getElementsByTagName("img");
+// console.log(target);
+
+// function changeList(){
+//   // imgをクリックしたらchangeListが開かれる
+//    console.log("aaa");
+  
+// }
+// target.addEventListener('click', changeList);
+
+
+
+// const list = document.getElementById("sample");
+// console.log(list); //null
+  
+const button = document.querySelector("button");
+console.log(button);
